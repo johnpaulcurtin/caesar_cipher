c46217472dba8bec3262968ba27d36f8c5df6b03
johnpaulcurtin <juanlove@gmail.com>
2025-09-22 14:28:39 -0700

    brainstorm/pseudocode basic lay out for method

---
+++ caesar_cipher.rb
@@ -0,0 +1,9 @@
+# split string to individual characters maintaining capitlization and spaces use #chars
+# convert string to number .ord will have to 
+# for each number add shift number after 25 number plus shift minus 25 (to wrap back to A)
+# convert back from number to string, rejoin with original spacing and capitlization
+
+def caesar_cipher(phrase, shift)
+ 
+  
+end
