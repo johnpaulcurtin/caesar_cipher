e6e9e260d27e0f6bb067d0b5c5b517cfefa9164d
johnpaulcurtin <juanlove@gmail.com>
2025-09-26 14:18:01 -0700

    create method for caeser shift add user input

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,28 @@
-# split string to individual characters maintaining capitlization and spaces use #chars
-# convert string to number .ord will have to 
-# for each number add shift number after 25 number plus shift minus 25 (to wrap back to A)
-# convert back from number to string, rejoin with original spacing and capitlization
+puts "Please input sentence: "
+phrase = gets.chomp
+puts "Please input shift: "
+shift = gets.chomp.to_i
 
-def caesar_cipher(phrase, shift)
- 
+
+
+def caesar_cipher(phrase, shift = nil)
+  cipher_array = phrase.chars.map do |char|
+    if char.match?(/[[:alpha:]]/)
+      if char.match?(/[[:upper:]]/)
+        ciph_ord = 65 + (char.ord + shift - 65) % 26
+        ciph_ord.chr
+      elsif char.match?(/[[:lower:]]/)
+        ciph_ord = 97 + (char.ord + shift - 97) % 26
+        ciph_ord.chr 
+        
+      end
+    else 
+      char
+
+    end
+  end
   
+  p cipher_array.join
 end
+
+caesar_cipher(phrase, shift)
